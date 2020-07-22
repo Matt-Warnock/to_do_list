381ce6b1ba17c3961d048846a762709b25650a47
MattW <drflamerock@yahoo.co.uk>
2020-07-22 15:01:34 +0100

    Refactored to reflect real dom enviorment pass second test

--- js/spec/toDoListUISpec.js
+++ js/spec/toDoListUISpec.js
@@ -2,12 +2,26 @@ describe('toDoListUI', function() {
   let toDoListUI;
 
   describe('when adding a to-do', function() {
-    let textInput = document.createElement('input');
+    let textInput = document.createElement('input'),
+        listArea = document.createElement('ul');
+
+    afterEach(function() {
+      var e = listArea;
+      var child = e.lastElementChild;
+        while (child) {
+            e.removeChild(child);
+            child = e.lastElementChild;
+          }
+      textInput.remove();
+      listArea.remove();
+    });
 
     it('adds a to-do item to the list', function() {
       document.body.appendChild(textInput);
+      document.body.appendChild(listArea);
       textInput.setAttribute('type', 'text');
       textInput.setAttribute('id', 'to_do');
+      listArea.setAttribute('id', 'list');
 
       toDoListUI = new ToDoListUI();
       toDoListUI.initialise();
@@ -17,9 +31,25 @@ describe('toDoListUI', function() {
       event.keyCode = 13;
       textInput.dispatchEvent(event);
 
-      let newItem = document.getElementById('item1');
+      expect(listArea.childNodes[0].textContent).toEqual('listen to Slayer');
+    });
 
-      expect(newItem.textContent).toEqual('listen to Slayer');
+    it('does not add item to the list if the input is empty', function() {
+      document.body.appendChild(textInput);
+      document.body.appendChild(listArea);
+      textInput.setAttribute('type', 'text');
+      textInput.setAttribute('id', 'to_do');
+      listArea.setAttribute('id', 'list');
+
+      toDoListUI = new ToDoListUI();
+      toDoListUI.initialise();
+
+      textInput.value = '';
+      let event = new Event('keydown');
+      event.keyCode = 13;
+      textInput.dispatchEvent(event);
+
+      expect(listArea.hasChildNodes()).toBe(false);
     });
   });
 });
--- js/src/toDoListUI.js
+++ js/src/toDoListUI.js
@@ -3,17 +3,19 @@ class ToDoListUI {
     this.toDoItem = '';
   }
   initialise() {
-    let input = document.getElementById('to_do');
+    let input = document.getElementById('to_do'),
+        listArea = document.getElementById('list');
 
     input.addEventListener('keydown', event => {
       if (event.keyCode === 13) {
         this.toDoItem = input.value;
 
-        let listItem = document.createElement('li');
-        document.body.appendChild(listItem);
-        listItem.setAttribute('id', 'item1');
-        listItem.textContent = this.toDoItem;
+        if (/\w{3,}/g.exec(this.toDoItem)) {
 
+          let listItem = document.createElement('li');
+          listArea.appendChild(listItem);
+          listItem.textContent = this.toDoItem;
+        }
       }
     });
   }
